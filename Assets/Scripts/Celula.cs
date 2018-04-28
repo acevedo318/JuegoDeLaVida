176067ade5aa1758b7c57cfff5e24e12fc77d0da
Kevin Alonso Acevedo Gonzalez <acevedo19972013@gmail.com>
2018-04-27 20:38:20 -0500

    InicialCommit

---
+++ Assets/Scripts/Celula.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Celula : MonoBehaviour {
+
+	public bool viva = true,vivaEnSiguienteGeneracion = false;
+	[SerializeField]
+	CelulaController controlCelula;
+	public GameObject infoCelulasVivas;
+
+
+	void awake(){
+		//controlCelula = GameObject.Find ("CelulasController").GetComponent<CelulaController>();
+	}
+
+	//Contador de vecinas
+	public int NumCelVecinasVivas(){
+
+		int numVecinasVivas = 0;
+		Collider2D[] vecinasVivas;
+		vecinasVivas = Physics2D.OverlapCircleAll (transform.position, 0.9f);
+
+		for (int i = 0; i < vecinasVivas.Length; i++) {
+			if(vecinasVivas[i].GetComponentInParent<Celula>().viva == true && vecinasVivas[i] != GetComponent<Collider2D>()){
+				numVecinasVivas++;
+			}
+
+		}
+
+
+		infoCelulasVivas.GetComponent<TextMesh> ().text = numVecinasVivas.ToString();
+
+		return numVecinasVivas;
+	}
+
+	// Use this for initialization
+	void Start () {
+		controlCelula = GameObject.Find ("CelulasController").GetComponent<CelulaController>();
+	}
+	
+	// Update is called once per frame
+	//Realizar pruebas
+	/*void FixedUpdate () {
+
+		NumCelVecinasVivas ();
+	}*/
+
+
+
+	public void Morir(){
+
+		GetComponent<SpriteRenderer> ().enabled = false;
+		viva = false;
+	}
+
+	public void Nacer(){
+
+		GetComponent<SpriteRenderer> ().enabled = true;
+		viva = true;
+	}
+
+
+	public void VidaSiguienteGeneracion(){
+		if (vivaEnSiguienteGeneracion) {
+			Nacer ();
+		} else {
+			Morir ();
+		}
+	}
+
+	//Presion de mouse activar boolean
+	void OnMouseDown(){
+
+
+		controlCelula.mouseDown = true;
+
+	}
+
+	//Nacer celula si se esta presionando
+	void OnMouseOver(){
+		if(controlCelula.mouseDown){
+			Nacer ();
+		}
+		//Detecta dentro del collider se presiono
+		if (Input.GetMouseButtonDown (1))
+			Morir();
+
+	}
+
+	//Desactivar bool
+	void OnMouseUp(){
+		controlCelula.mouseDown = false;
+	}
+
+}
+
---
+++ Assets/Scripts/CelulaController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CelulaController : MonoBehaviour {
+
+	private bool empezar = false;
+	[SerializeField]
+	GameObject celulaPrefab,ContenedorCelula;
+	//guardar cuadricula
+	private List<Celula> celulas = new List<Celula> ();
+	public float tiempoGeneracion = 1f;
+
+	public bool mouseDown;//Variable global de presion de mouse 
+	public Text txtGeneracion;
+	private int generacion = 0;
+
+	void Awake(){
+		GameObject.Find ("CelulasContent");
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		generacion = 0;
+		CrearCelula ();
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ActivarPanel(GameObject panel){
+
+		if (panel.activeSelf) {
+			panel.SetActive (false);
+		} else {
+			panel.SetActive (true);
+		}
+	
+
+
+	}
+
+	public void PlayPause(){
+
+		empezar = !empezar;
+		if (empezar) {
+			SiguienteGeneracion ();
+		} else {
+			CancelInvoke ("SiguienteGeneracion");
+		}
+
+	}
+
+
+	//Funcion para invocar siguiente generacion se invoca cada vez que termine y el tiempo timeGeneracion
+	//Tiene en cuenta las reglas del juego
+	void SiguienteGeneracion(){
+
+		foreach (var item in celulas) {
+
+			if (item.viva) {
+
+				if (item.NumCelVecinasVivas() == 2 || item.NumCelVecinasVivas() == 3) {
+
+					item.vivaEnSiguienteGeneracion = true;
+
+				} else {
+					item.vivaEnSiguienteGeneracion = false;
+				}
+
+			} else {
+
+				if (item.NumCelVecinasVivas() == 3) {
+					item.vivaEnSiguienteGeneracion = true;
+				} else {
+					
+				}
+
+			}
+
+		}
+
+		foreach (var item in celulas) {
+			item.VidaSiguienteGeneracion ();
+		}
+		generacion++;
+		txtGeneracion.text = "\n"+generacion;
+		Invoke ("SiguienteGeneracion",tiempoGeneracion);
+
+	}
+
+
+	//Crea las 10000 celulas
+	void CrearCelula(){
+		
+		for (int i = 0; i < 50; i++) {
+				
+			for (int j = 0; j < 50; j++) {
+
+				GameObject celulaTemp = Instantiate (celulaPrefab, new Vector2 (j, i), Quaternion.identity);
+				celulas.Add (celulaTemp.GetComponent<Celula>());
+				celulaTemp.GetComponent<Celula> ().Morir ();
+				celulaTemp.transform.parent = ContenedorCelula.transform;
+			}
+
+		}
+
+		ContenedorCelula.SetActive (true);
+
+	}
+
+}
